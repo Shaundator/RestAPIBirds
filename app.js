f3c096b1ed77d4c7d8fc0ba829d0e3b4bfa59fe9
Shaundator <rash0081@stud.kea.dk>
2023-02-20 14:25:17 +0100

    more

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const app = require("express")();
 
 const express = require("express")();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json())
+
 app.listen(8080);
 
-// simulating some database
+// simulating some database2
 
-const birds = [];
-birds.push([1, "one bird"]);
-birds.push([2, "two bird"]);
-birds.push([3, "three bird"]);
-birds.push([4, "four bird"]);
-birds.push([5, "five bird"]);
-
-
+const birds = [
+    {id: 1, name: "First Bird"},
+    {id: 2, name: "Second Bird"},
+    {id: 3, name: "Third Bird"},
+    {id: 4, name: "Fourth Bird"},
+    {id: 5, name: "Fifth Bird"}
+];
 
 // get all
 app.get("/birds", (req, res) => {
@@ -22,39 +26,55 @@ app.get("/birds", (req, res) => {
 // get
 app.get("/birds/:id", (req, res) => {
     const id = req.params.id
-    birds.forEach(bird => {
-        if(bird[0] == id ){
-            console.log('bird found: ' + bird)
-            res.send(bird)
-        }
-    });
+    const foundBird = birds.find((bird) => bird.id == id)
+    res.send(foundBird)
 })
 
 // get by name
 app.get("/birds/findByName/:name", (req, res) => {
     const name = req.params.name
-    birds.forEach(bird => {
-        if(bird[1] == name){
-            console.log('bird found: ' + bird)
-            res.send(bird)
-        }
-    }) 
+    const foundBird = birds.find((bird) => bird.name == name)
+    res.send(foundBird)
+
 })
 
 // post
-app.get("/birds/:bird", (req, res) => {
-
+app.post("/birds", (req, res) => {
+    const postBird = req.body
+    birds.push(postBird)
+    res.send(postBird)
 })
+
 // put
-app.get("/birds/:bird", (req, res) => {
-
+app.put("/birds/:id", (req, res) => {
+    const oldBird = birds.find((bird) => bird.id === req.params.id)
+    if(oldBird) {
+        res.status(404).send("No birds here")
+    } else {
+        const birdIndex = birds.findIndex((bird) => bird.id == req.params.id);
+        birds[birdIndex] = req.body
+        res.send(birds[birdIndex])
+    }
 })
-// patch
-app.get("/birds/:bird", (req, res) => {
 
+app.patch("/birds/:id/:field", (req, res) => {
+    const oldBird = birds.find((bird) => bird.id == req.params.id)
+    if(oldBird) {
+        res.status(404).send("No birds here")
+    } else {
+        res.send("to be made")
+    }
 })
-
 // delete
-app.get("/birds/:id", (req, res) => {
+app.delete("/birds/:id", (req, res) => {
+    const birdId = parseInt(req.params.id)
+    const birdIndex = birds.findIndex((bird) => bird.id === birdId)
+
+    if(birdIndex === -1){
+        res.status(404).send("No birds here")
+    } else {
+        birds.splice(birdIndex, 1)
+        res.send("bird with ID " + birdId + " has been deleted.")
+    }
     
 })
